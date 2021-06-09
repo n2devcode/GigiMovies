9756797122ccdd39845ed60bf77ca89fe9b0c91e
Anna Noé Julián <n2dev.code@gmail.com>
2021-06-09 15:40:51 +0200

    ADD tabs in main screen

--- GigiMovies/ViewController.swift
+++ GigiMovies/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var buttonAll: UIButton!
+    @IBOutlet weak var buttonFavorites: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadAllMovies()
     }
 
-
+    @IBAction func clickAll(_ sender: Any) {
+        loadAllMovies()
+    }
+    
+    @IBAction func clickFavorites(_ sender: Any) {
+    }
+    
+    private func loadAllMovies() {
+        
+    }
 }
 
